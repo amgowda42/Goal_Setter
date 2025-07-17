ff245d2371f061602eca2f93271b6bd90ed1a99b
Annappa Gowda <annappag2020@gmail.com>
2025-07-18 02:00:15 +0530

    initial test of server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+const express = require("express");
+const port = process.env.PORT || 8000;
+
+require("dotenv").config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
