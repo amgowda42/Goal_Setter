0fe4420bb184e7cb91990c09d5122962d730f5d6
Annappa Gowda <annappag2020@gmail.com>
2025-08-25 23:34:38 +0530

    user schema update

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -14,6 +14,11 @@ const userSchem = new mongoose.Schema(
     password: {
       type: String,
       required: [true, "Please add a password"],
+      minlength: [6, "Password must be at least 6 characters long"],
+      match: [
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z\d]).{6,}$/,
+        "Password must contain at least one letter, one number, and one special character",
+      ],
     },
     phoneNumer: {
       type: Number,
