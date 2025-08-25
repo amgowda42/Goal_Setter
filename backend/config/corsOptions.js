f98767da6d88629f1fb299c4c51b18739bc7ccc1
Annappa Gowda <annappag2020@gmail.com>
2025-08-25 22:31:38 +0530

    updates in auth middle ware

---
+++ backend/config/corsOptions.js
@@ -0,0 +1,17 @@
+// Define whitelist of allowed origins
+const allowedOrigins = ["http://localhost:3000"];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,13 @@ const port = process.env.PORT || 8000;
 const connectDB = require("./config/db.js");
 const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware.js");
+const corsOptions = require("./config/corsOptions.js");
+const cors = require("cors");
 
 connectDB();
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
